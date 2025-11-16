494c53dd6c258c564993fad5301fdf854e0e97ff
inertech <inertech@inertech.com>
2025-11-16 17:22:04 +0100

    set angularMaterialApp as main route

--- projects/angular-material-shell/src/app/app.routes.ts
+++ projects/angular-material-shell/src/app/app.routes.ts
@@ -1,15 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { loadRemoteModule } from '@softarc/native-federation-runtime';
 
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    component: HomeComponent,
+    loadComponent: () => loadRemoteModule('angularMaterialApp', './Component'),
   },
-  {
-    path: 'angularMaterialApp',
-    loadComponent: () => loadRemoteModule('angularMaterialApp', './Component')
-  }
 ];
